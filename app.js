b5e9725c67b4e42daea3551ded62cd1b3985e3fc
Nischal Singh <singh.nischal.99@gmail.com>
2021-09-14 22:15:09 +0530

    App Complete

--- app.js
+++ app.js
@@ -2,9 +2,28 @@ var cp=document.querySelector('#initial-price');
 var quantity=document.querySelector('#stocks-quantity');
 var sp=document.querySelector('#current-price');
 var button=document.querySelector('#submit-btn');
+var outputBox=document.querySelector('#output-box');
 // cp=Number(cp.value);
 // quantity=Number(quantity.value);
 // sp=Number(sp.value);
+function calculate(costPrice,sellingPrice)
+{
+    if(costPrice>sellingPrice)
+    {
+        var loss=costPrice-sellingPrice;
+        var lossPer=(loss/costPrice)*100;
+        showMessage(`You have suffered a loss of ${loss} and loss % is ${lossPer}`,1);
+    }
+    else if(sellingPrice>costPrice)
+    {
+        var profit=sellingPrice-costPrice;
+        var profitPer=(profit/costPrice)*100;
+        showMessage(`You have gained a profit of ${profit} and profit % is ${profitPer}`,0);
+    }
+    else{
+        showMessage(`NO profit No loss`);
+    }
+}
 button.addEventListener('click', function call(){
     // console.log("CP "+typeof(cp.value));
     // console.log("SP "+sp.value);
@@ -12,5 +31,17 @@ button.addEventListener('click', function call(){
     var initial=Number(cp.value);
     var num=Number(quantity.value);
     var current=Number(sp.value);
-    console.log(initial+" "+num+" "+current);
+    calculate(initial*num,current*num);
 })
+function showMessage(message,flag) {
+    outputBox.innerHTML = message;
+    console.log(flag);
+    if(flag==1)
+    {
+        outputBox.style.color='red';
+    }
+    else
+    {
+        outputBox.style.color='green';
+    }
+  }
